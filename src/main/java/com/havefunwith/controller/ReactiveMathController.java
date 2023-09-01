c257349db9aa48c2daa5151720330ca39ce2bfb8
mrdbrg <rdg.brg@gmail.com>
2023-09-01 15:07:24 -0500

    endpoints for non-blocking asynchronous requests / read requests in a non-blocking way on Post

---
+++ src/main/java/com/havefunwith/controller/ReactiveMathController.java
@@ -0,0 +1,43 @@
+package com.havefunwith.controller;
+
+import com.havefunwith.dto.MultiplyRequestDto;
+import com.havefunwith.dto.Response;
+import com.havefunwith.service.ReactiveMathService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("reactive-math")
+@RequiredArgsConstructor
+public class ReactiveMathController {
+
+    private final ReactiveMathService reactiveMathService;
+
+    @GetMapping("square/{input}")
+    public Mono<Response> findSquare(@PathVariable int input) {
+        return reactiveMathService.findSquare(input);
+    }
+
+    @GetMapping("table/{input}")
+    public Flux<Response> multiplicationTable(@PathVariable int input) {
+        return reactiveMathService.multiplicationTable(input);
+    }
+
+    @GetMapping(value = "table/{input}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Response> multiplicationTableStream(@PathVariable int input) {
+        return reactiveMathService.multiplicationTable(input);
+    }
+
+    @PostMapping("multiply")
+    public Mono<Response> multiply(@RequestBody Mono<MultiplyRequestDto> requestDtoMono,
+                                   @RequestHeader Map<String, String> headers) {
+        System.out.println("HEADERS :: =====> " + headers);
+        return reactiveMathService.multiply(requestDtoMono);
+    }
+
+}
